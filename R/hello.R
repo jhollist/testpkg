05c89409970241c764dba5b34830e33b7b9baedd
Jeff Hollister <jeff.w.hollister@gmail.com>
2020-04-14 16:17:29 -0400

    does just sf work?

--- R/hello.R
+++ R/hello.R
@@ -1,12 +1,11 @@
 #' Function
 #' @importFrom sf st_sfc st_point
-#' @importFrom lwgeom st_astext
 #' @export
+#' @examples
+#' hello()
 
 hello <- function() {
   pt <- st_sfc(st_point(c(1.0002,2.3030303)), crs = 4326)
-  st_astext(pt, 3)
-  print("Hello, world!")
-
+  pt
 }
 
