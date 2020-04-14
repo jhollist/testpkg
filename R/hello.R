676fa7d8a6f954390c95fad89319bbe21f03270f
Jeff Hollister <jeff.w.hollister@gmail.com>
2020-04-14 16:35:00 -0400

    add lwgeom, becuase sf works

--- R/hello.R
+++ R/hello.R
@@ -9,3 +9,12 @@ hello <- function() {
   pt
 }
 
+#' Function 2
+#' @importFrom lwgeom st_astext
+#' @param pt a point
+#' @export
+#' @examples
+#' bye(hello())
+bye <- function(pt){
+  st_astext(pt)
+}
