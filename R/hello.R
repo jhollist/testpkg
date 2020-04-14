d23960046078079c691b4e3707100166105bf778
Jeff Hollister <jeff.w.hollister@gmail.com>
2020-04-14 14:43:49 -0400

    new rcmdcheck

--- R/hello.R
+++ R/hello.R
@@ -1,11 +1,11 @@
 #' Function
-#' @import sf
-#' @import lwgeom
+#' @importFrom sf st_sfc st_point
+#' @importFrom lwgeom st_astext
 #' @export
 
 hello <- function() {
-  pt <- sf::st_sfc(sf::st_point(c(1.0002,2.3030303)), crs = 4326)
-  lwgeom::st_astext(pt, 3)
+  pt <- st_sfc(st_point(c(1.0002,2.3030303)), crs = 4326)
+  st_astext(pt, 3)
   print("Hello, world!")
 
 }
